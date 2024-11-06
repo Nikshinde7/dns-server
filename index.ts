02a22c2367b07c1b11a3eac538e32bb8973712d2
Nikshinde7 <shindenikhil336@gmail.com>
2024-11-06 17:55:30 +0530

    fea: Added the basic db for testing the stuff out

--- index.ts
+++ index.ts
@@ -1,9 +1,45 @@
 import * as dgram from "dgram";
+import * as dpacket from "dns-packet"
 
 const server: dgram.Socket = dgram.createSocket('udp4');
 
+// This is just a basic DB implementation to mimic the DNS behaviour
+const db: any = {
+    "nikshinde.dev": {
+        type: "A",
+        data: "1.2.3.4"
+    },
+    "gear.nikshinde.dev": {
+        type: "A",
+        data: "4.5.6.7"
+    }
+}
+
+/*
+ The msg we get here comes in binary format
+ and the second parameter which is remoteInfo, provides info about who queried the DNS
+*/
 server.on('message', (msg, remoteInfo) => {
-    console.log('Incoming Message', msg.toString())
+    const incomingReq: dpacket.DecodedPacket = dpacket.decode(msg)
+
+    if(!incomingReq.questions) return ''
+
+    const ipFromDb = db[incomingReq.questions[0].name];
+
+    const answer = dpacket.encode({
+        id: incomingReq.id,
+        type: 'response',
+        flags: dpacket.AUTHORITATIVE_ANSWER,
+        questions: incomingReq.questions,
+        answers: [{
+            type: ipFromDb.type,
+            class: "IN",
+            name: incomingReq.questions[0].name,
+            data: ipFromDb.data
+        }] 
+    });
+
+    server.send(answer, remoteInfo.port, remoteInfo.address)
 })
 
 server.bind(2053, "127.0.0.1", () => {
